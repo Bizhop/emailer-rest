070af4b9f6c718a2a9669bcba5fb4b55d2e3bf73
Ville Piispa <ville.piispa@gmail.com>
2023-07-12 11:39:12 +0300

    add formatted timestamp to Email model

--- src/main/java/fi/bizhop/emailerrest/model/Email.java
+++ src/main/java/fi/bizhop/emailerrest/model/Email.java
@@ -1,5 +1,6 @@
 package fi.bizhop.emailerrest.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
@@ -13,5 +14,6 @@ public class Email {
     @NonNull String to;
     @NonNull String subject;
     String content;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     ZonedDateTime timestamp;
 }
